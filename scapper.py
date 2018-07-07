0c70d16758df42e27a14aa8dfc73b8c0058bced4
mling <sapient007@hotmail.com>
2018-07-07 18:48:12 -0400

    updated

--- scapper.py
+++ scapper.py
@@ -5,9 +5,9 @@ import json
 import yagmail
 
 #define some seach basic paramerters
-MODEL = '860'
-SIZE = '9.5'
-WIDTH = 'W'
+#MODEL = '860'
+#SIZE = '9.5'
+#WIDTH = 'W'
 TARGET_PRICE = '50'
 EMAIL = ''
 
@@ -17,14 +17,28 @@ BASE_URL= ''
 def generateBaseURL():
     BASE_URL = 'https://www.joesnewbalanceoutlet.com/products/?Filters%5BSize%5D=9.5&Filters%5BWidth%5D=W&PriceRange=&OnSale=&Icon=&Brand=0&PageSize=24&Page=1&Branded=False&ListType=Grid&Text=860&Sorting=Newest'
 
-def parsePage():
+def parse_page_for_price(quote_page):
     page = urllib2.urlopen(quote_page)
     soup = BeautifulSoul(page, 'html.parser')
     #check to see if shoes are below the target price <to be implemented>
+    return price
 
 
-def notify():
+def notify_me():
     yag = yagmail.SMTP()
     contents = ['found you a cheap shoe, here is the link' BASE_URL ]
     yag.send(EMAIL, 'new balance found', contents)
 
+
+def main()
+    # Check the page
+    price = parse_page_for_price()
+
+    # Check the page
+    if price < threshold_price:
+        notify_me()
+
+
+if __name__ == "__main__":
+    # TODO: Run scheduler once a day
+    main()
