13200d9ecac6fddb8d105ef99738b3c99bf343b1
mling <sapient007@hotmail.com>
2018-07-06 22:56:24 -0400

    new commit

---
+++ scapper.py
@@ -0,0 +1,32 @@
+# import libraries
+import urllib2
+from bs4 import BeautifulSoup
+import json
+import yagmail
+
+#define some seach basic paramerters
+MODEL = '860'
+SIZE = '9.5'
+WIDTH = 'W'
+TARGET_PRICE = '50'
+
+# specify the url
+BASE_URL= ''
+
+def generateBaseURL():
+    BASE_URL = 'https://www.joesnewbalanceoutlet.com/products/?Filters%5BSize%5D=9.5&Filters%5BWidth%5D=W&PriceRange=&OnSale=&Icon=&Brand=0&PageSize=24&Page=1&Branded=False&ListType=Grid&Text=860&Sorting=Newest'
+
+def parsePage():
+    page = urllib2.urlopen(quote_page)
+    soup = BeautifulSoul(page, 'html.parser')
+    #check to see if shoes are below the target price <to be implemented>
+
+
+
+def notify(shoe):
+    yag = yagmail.SMTP()
+    contents = [' found you a cheap shoe']
+    yag.send('mingeling@gmail.com', 'new balance found', contents)
+
+
+
