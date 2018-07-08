c6d64f0c925dfc0bb46740053ef07fab01d1c59e
mling <sapient007@hotmail.com>
2018-07-07 22:39:20 -0400

    changed screen scrapper to show lowest price

--- scapper.py
+++ scapper.py
@@ -1,5 +1,5 @@
 # import libraries
-import urllib2
+from urllib import request
 from bs4 import BeautifulSoup
 import json
 import yagmail
@@ -8,37 +8,37 @@ import yagmail
 #MODEL = '860'
 #SIZE = '9.5'
 #WIDTH = 'W'
-TARGET_PRICE = '50'
+TARGET_PRICE = 50
 EMAIL = ''
 
 # specify the url
-BASE_URL= ''
+BASE_URL= 'https://www.joesnewbalanceoutlet.com/men/shoes/running/?Filters%5BSize%5D=9.5&Filters%5BWidth%5D=2E&Categories=men&Categories=shoes&Categories=running&PriceRange=&OnSale=&Icon=&Brand=0&PageSize=24&Page=1&Branded=False&ListType=Grid&Text=1080&Sorting=LowestPrice'
 
 def generateBaseURL():
     BASE_URL = 'https://www.joesnewbalanceoutlet.com/men/shoes/running/?Filters%5BSize%5D=9.5&Filters%5BWidth%5D=2E&Categories=men&Categories=shoes&Categories=running&PriceRange=&OnSale=&Icon=&Brand=0&PageSize=24&Page=1&Branded=False&ListType=Grid&Text=1080&Sorting=LowestPrice'
 
 def parse_page_for_price(quote_page):
-    page = urllib2.urlopen(quote_page)
-    soup = BeautifulSoul(page, 'html.parser')
+    page = request.urlopen(quote_page)
+    soup = BeautifulSoup(page, 'html.parser')
     #scape the screen and find the lowest price posted
     price_div = soup.find('div', attrs={'class': 'productPrice'})
-    price = price_div.text.strip()
-    print price
-    return price
+    price = price_div.text.strip().replace("$", "")
+    print(price)
+    return float(price)
 
 
 def notify_me():
     yag = yagmail.SMTP()
-    contents = ['found you a cheap shoe, here is the link' BASE_URL ]
+    contents = ['found you a cheap shoe, here is the link' + BASE_URL ]
     yag.send(EMAIL, 'new balance found', contents)
 
 
-def main()
+def main():
     # Check the page
-    price = parse_page_for_price()
+    price = parse_page_for_price(BASE_URL)
 
     # Check the page
-    if price < threshold_price:
+    if price < TARGET_PRICE:
         notify_me()
 
 
