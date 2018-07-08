c7e47cc8a37c384fa646003b0b0427f97d376b01
mling <sapient007@hotmail.com>
2018-07-08 09:51:20 -0400

    added manifest and pull env variables from system

--- scapper.py
+++ scapper.py
@@ -1,18 +1,22 @@
 # import libraries
 from urllib import request
 from bs4 import BeautifulSoup
+import os
 import json
 import yagmail
 
-#define some seach basic paramerters
-MODEL = '1080'
-SIZE = '9.5'
-WIDTH = '2E'
-TARGET_PRICE = 50
-EMAIL = ''
+#define some deafult paramerters
+MODEL = os.getenv("MODEL", '1080')
+SIZE = os.getenv("SIZE", '9.5')
+WIDTH = os.getenv("WIDTH", '2E')
+TARGET_PRICE = os.getenv("PRICE", 50)
+EMAIL = os.getenv("EMAIL", 'sapient007@hotmail.com')
+SMTP_USER = os.getenv("SMTP_USER") #no defaults are set for this 
+SMTP_PASS = os.getenv("SMTP_PASS") #no defaults are set for this 
 
 # specify the url
 BASE_URL= 'https://www.joesnewbalanceoutlet.com/men/shoes/running/?Filters%5BSize%5D=' + SIZE + '&Filters%5BWidth%5D=' + WIDTH + '&Categories=men&Categories=shoes&Categories=running&PriceRange=&OnSale=&Icon=&Brand=0&PageSize=24&Page=1&Branded=False&ListType=Grid&Text=' + MODEL + '&Sorting=LowestPrice'
+
 
 def parse_page_for_price(quote_page):
     page = request.urlopen(quote_page)
@@ -35,12 +39,18 @@ def notify_me():
     yag.send(EMAIL, 'new balance found', contents)
 
 
+def schedule():
+    # TODO: to implement a scheduler assocaited with this 
+    pass
+
+
 def main():
     # Check the page
     price = parse_page_for_price(BASE_URL)
     if price is not None:
         # Check the page
         if price < TARGET_PRICE:
+            print("sending mail")
             notify_me()
 
 if __name__ == "__main__":
